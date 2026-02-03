66492c04a669b9f4bc62589c81b00ea6c464256b
Vincent <git@varmail.org>
2026-02-03 10:35:17 -0800

    added library and code to fetch video title and made a groundwork for the database

--- program.py
+++ program.py
@@ -1,5 +1,11 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
 from youtube_transcript_api import YouTubeTranscriptApi
+from pytubefix import YouTube as yt_info_grab
+
+import sqlite3
+
+DB_PATH = 'results.db'
 
 def extract_transcript_text(transcript_data: list[dict], join_with: str = " ") -> str:
     """
@@ -16,15 +22,23 @@ def extract_transcript_text(transcript_data: list[dict], join_with: str = " ") -
     text_segments = [entry['text'].replace('\n', ' ') for entry in transcript_data if 'text' in entry]
     return join_with.join(text_segments)
 
-videos = {
-    "MIT Chemistry Lecture" : "aKTOS0Nrlug",
-    "Mr. Beast Video" : "pAnGwRiQ4-4",
-    "Behavioral Evlotion Stanford Video" : "Y0Oa4Lp5fLE",
-    "Stanford Introduction to Computers" : "di0KgqNDqhA"
-}
+def init_db():
+    with sqlite3.connect(DB_PATH) as conn:
+        conn.execute("""
+            CREATE TABLE IF NOT EXISTS results (
+                video_id TEXT NOT NULL,
+                text TEXT NOT NULL,
+                score REAL NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+        """)
+
+video_ids = ["aKTOS0Nrlug", "pAnGwRiQ4-4", "Y0Oa4Lp5fLE", "di0KgqNDqhA", "_C-ZzlGS8Vk", "HAnw168huqA"]
 
 ytt_api = YouTubeTranscriptApi()
-yt_fetch = ytt_api.fetch(videos.get("Mr. Beast Video"))
+
+yt_info = yt_info_grab(f"https://www.youtube.com/watch?v={video_ids[0]}")
+yt_fetch = ytt_api.fetch(video_ids[0], languages=['en', 'en-US'])
 
 tokenizer = AutoTokenizer.from_pretrained("HuggingFaceTB/fineweb-edu-classifier")
 model = AutoModelForSequenceClassification.from_pretrained("HuggingFaceTB/fineweb-edu-classifier")
@@ -34,11 +48,12 @@ inputs = tokenizer(str(text), return_tensors="pt", padding="longest", truncation
 outputs = model(**inputs)
 logits = outputs.logits.squeeze(-1).float().detach().numpy()
 score = logits.item()
+
 result = {
     "text": text,
-    "video_title": yt_fetch.video_id,
+    "video_id": yt_fetch.video_id,
+    "video_title": yt_info.title,
     "score": score,
-    "int_score": int(round(max(0, min(score, 5)))),
 }
 
 print(result)
